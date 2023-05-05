41a9010aa0a0b88af06238e7f7d7262c8227ef47
kreininmv <kreinin.mv@phystech.edu>
2023-05-05 22:41:28 +0300

    add b of second problem in 9-th homework

--- Homework/HW_9.py
+++ Homework/HW_9.py
@@ -54,9 +54,87 @@ class Optimizer:
             return w - lr*np.linalg.inv(self.hessian_func(w, self.args)) @ self.grad_func(w, self.args)
         return w - lr*self.grad_func(w, self.args) / self.hessian_func(w, self.args)
     
+    def broyden(self, w, H, k):
+        grad_prev = self.grad_func(w, self.args)
+        d = - H @ grad_prev
+        lr = self.learning_rate(w, self.args)
+        w, w_prev = w + lr * d, w
+        grad = self.grad_func(w, self.args)
+        s = w - w_prev
+        y = grad - grad_prev
+        q = s - H @ y
+        mu = 1 / (q @ y)
+        H = H + mu * np.array([q]).T @ np.array([q])
+        
+        return w, H
+    
+    def dfp(self, w, H, k):
+        grad_prev =  self.grad_func(w, self.args)
+        d = - H @ grad_prev
+        lr = self.learning_rate(w, self.args)
+        w, w_prev = w + lr * d, w
+        grad = self.grad_func(w, self.args)
+        s = np.array([w - w_prev]).T
+        y = np.array([grad - grad_prev]).T
+        
+        H = H - (H @ y @ y.T @ H) / (y.T @ H @ y) + s @ s.T / (y.T @ s)
+
+        return w, H, w_prev
+    
+    def bfgs(self, w, H, k):
+        grad_prev = self.grad_func(w, self.args)
+        
+        d = - H @ grad_prev
+        lr = self.learning_rate(w, self.args)
+        w, w_prev = w + lr * d, w
+        grad = self.grad_func(w, self.args)
+        
+        s = np.array([w - w_prev]).T
+        y = np.array([grad - grad_prev]).T
+        
+        rho = 1 / (y.T @ s)
+        V = np.eye(len(w)) - rho * y @ s.T
+        H = V.T @ H @ V + rho * s @ s.T
+        
+        return w, H, w_prev
+    
+
+    def lbfgs(self, w, H, H0, ss, ys, k):
+        grad_prev = self.grad_func(w, self.args)
+
+        d = - H @ grad_prev
+        lr = self.learning_rate(w, self.args)
+        w, w_prev = w + lr * d, w
+        grad = self.grad_func(w, self.args)
+
+        s = w - w_prev
+        y = grad - grad_prev
+        m = self.args['m']
+        
+        for i in range(m - 1):
+            ss[ : , i] = ss[ : , i + 1]
+            ys[ : , i] = ys[ : , i + 1]
+        ss[ : , m - 1] = s
+        ys[ : , m - 1] = y
+
+        H = H0
+        for i in range(max(0, m - k - 1), m):
+            s = np.array([ss[ : , i ]]).T
+            y = np.array([ys[ : , i ]]).T
+            rho = 1 / (y.T @ s)
+            V = np.eye(len(w)) - rho * y @ s.T
+            H = V.T @ H @ V + rho * s @ s.T
+        return w, H, ss, ys
+
     def fit(self):
         to_seconds = lambda s: s.microseconds * 1e-6 + s.seconds
         time_start = dt.now()
+        
+        H = self.hessian_func(self.w, self.args)
+        H0 = np.copy(H)
+        ss = np.zeros((len(self.w), 3))
+        ys = np.zeros((len(self.w), 3))
+
         for k in range(self.iter):
             w_prew = self.w
             if self.name == 'gd':
@@ -65,7 +143,15 @@ class Optimizer:
                 self.w = self.newton(self.w, k)
             elif self.name == 'cubicnewton':
                 self.w = self.cubic_newton(self.w, k)
-        
+            elif self.name == 'dfp':
+                self.w, H, w_prew = self.dfp(self.w, H, k)
+            elif self.name  == 'bfgs':
+                self.w, H, w_prew = self.bfgs(self.w, H, k)
+            elif self.name == 'lbfgs':
+                self.w, H, ss, ys = self.lbfgs(self.w, H, H0, ss, ys, k)
+            elif self.name == 'broyden':
+                self.w, H = self.broyden(self.w, H, k)
+
             self.args['w_prev'] = w_prew
             if self.args['test']:
                 error = np.linalg.norm(self.grad_func(self.w, self.args), 2)
